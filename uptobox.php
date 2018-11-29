3858323f7cd256a587a0449a398bd3313b4b6661
andri85 <45422371+andri85@users.noreply.github.com>
2018-11-29 10:15:10 +0100

    Add files via upload

---
+++ uptobox.php
@@ -0,0 +1,49 @@
+<?php
+$token = "[USER_TOKEN]";
+
+$code="";
+if ($_GET['code'] != null && $_GET['code'] != ""){
+	$code = $_GET['code'];
+}
+
+$type="json";
+if ($_GET['type'] != null && $_GET['type'] != ""){
+	$type = $_GET['type'];
+}
+
+$url="https://uptobox.com/api/link?token=".$token."&id=".$code;
+$return = file_get_contents($url);
+
+$jsonData = json_decode($return, true);
+$esito = $jsonData['statusCode'];
+$link = "";
+$message = "";
+if ($esito=="0"){
+	$data = $jsonData['data'];
+	$link = $data['dlLink'];
+	$jsData = explode("/", $link);
+	$name = urldecode(end($jsData));
+}else{
+	$message = $jsonData['message'];
+}
+
+$toRet = "";
+if ($link == ""){
+	$link = "http://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_640_3MG.mp4";
+	$name = $message;
+}
+if ($type == "json"){
+	$toRet = '{"link": "'.$link.'", "name": "'.$name.'"}';
+}
+if ($type == "html"){
+	$toRet = '<a href="'.$link.'">'.$name.'</a>';
+}
+if ($type == "m3u"){
+	$toRet = "#EXTM3U\n\n#EXTINF:-1,".$name."\n".$link;
+	header ("Content-type: application/x-mpegURL");
+	header ('Content-Disposition: attachment; filename="MovieUpt.m3u"');
+	header ("Pragma: no-cache");
+	header ("Expires: 0");
+}
+echo $toRet;
+?>
