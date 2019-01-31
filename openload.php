a65f91e2d58438447f2c5b77ce4e43dd04927395
andri85 <45422371+andri85@users.noreply.github.com>
2019-01-31 21:14:24 +0100

    Add files via upload

---
+++ openload.php
@@ -0,0 +1,52 @@
+<?php
+	$token = "e88956dd8d597979";
+	if ($_GET['token'] != null && $_GET['token'] != ""){
+		$token = $_GET['token'];
+	}
+	
+	$code="";
+	if ($_GET['code'] != null && $_GET['code'] != ""){
+		$code = $_GET['code'];
+	}
+	
+	$type="json";
+	if ($_GET['type'] != null && $_GET['type'] != ""){
+		$type = $_GET['type'];
+	}
+	
+	$url="https://api.openload.co".$token."&id=".$code;
+	$return = file_get_contents($url);
+	
+	$jsonData = json_decode($return, true);
+	$esito = $jsonData['statusCode'];
+	$link = "";
+	$message = "";
+	if ($esito=="0"){
+		$data = $jsonData['data'];
+		$link = $data['dlLink'];
+		$jsData = explode("/", $link);
+		$name = urldecode(end($jsData));
+	}else{
+		$message = $jsonData['message'];
+	}
+	
+	$toRet = "";
+	if ($link == ""){
+		$link = "";
+		$name = $message;
+	}
+	if ($type == "json"){
+		$toRet = '{"link": "'.$link.'", "name": "'.$name.'"}';
+	}
+	if ($type == "html"){
+		$toRet = '<a href="'.$link.'">'.$name.'</a>';
+	}
+	if ($type == "m3u"){
+		$toRet = "#EXTM3U\n\n#EXTINF:-1,".$name."\n".$link;
+		header ("Content-type: application/x-mpegURL");
+		header ('Content-Disposition: attachment; filename="MovieUpt.m3u"');
+		header ("Pragma: no-cache");
+		header ("Expires: 0");
+	}
+	echo $toRet;
+	?>
